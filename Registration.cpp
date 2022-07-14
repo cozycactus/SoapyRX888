f836586798a6fb21b20feb5aefb047a962fbe065
Ruslan Migirov <trapi78@gmail.com>
2022-07-14 16:21:36 +0300

    Save

--- Registration.cpp
+++ Registration.cpp
@@ -13,6 +13,37 @@ SoapySDR::KwargsList findRX888(const SoapySDR::Kwargs &args)
 {
     //locate the device on the system...
     //return a list of 0, 1, or more argument maps that each identify a device
+
+    std::vector<SoapySDR::Kwargs> results;
+
+    char manufact[256], product[256], serial[256];
+
+    const size_t this_count = rx888_get_device_count();
+
+    for (size_t i = 0; i < this_count; i++)
+    {
+        if (rx888_get_device_usb_strings(i, manufact, product, serial) != 0)
+        {
+            SoapySDR_logf(SOAPY_SDR_ERROR, "rx888_get_device_usb_strings(%zu) failed", i);
+            continue;
+        }
+        SoapySDR_logf(SOAPY_SDR_DEBUG, "\tManufacturer: %s, Product Name: %s, Serial: %s", manufact, product, serial);
+
+        SoapySDR::Kwargs devInfo;
+        devInfo["label"] = std::string(rx888_get_device_name(i)) + " :: " + serial;
+        devInfo["product"] = product;
+        devInfo["serial"] = serial;
+        devInfo["manufacturer"] = manufact;
+        
+
+        //filtering by serial
+        if (args.count("serial") != 0 and args.at("serial") != serial) continue;
+
+        results.push_back(devInfo);
+    }
+
+    return results;
+
 }
 
 
--- Settings.cpp
+++ Settings.cpp
@@ -6,8 +6,27 @@
 
 
 SoapyRX888::SoapyRX888(const SoapySDR::Kwargs &args):
-
+    deviceId(-1),
+    dev(nullptr),
+    rxFormat(RX888_RX_FORMAT_INT16),
+    sampleRate(64000000)
 {
+    if (args.count("label") != 0) SoapySDR_logf(SOAPY_SDR_INFO, "Opening %s...", args.at("label").c_str());
+
+    //if a serial is not present, then findRTLSDR had zero devices enumerated
+    if (args.count("serial") == 0) throw std::runtime_error("No RX888 devices found!");
+
+    const auto serial = args.at("serial");
+    deviceId = rx888_get_index_by_serial(serial.c_str());
+    if (deviceId < 0) throw std::runtime_error("rx888_get_index_by_serial("+serial+") - " + std::to_string(deviceId));
+
+    
+    
+    SoapySDR_logf(SOAPY_SDR_DEBUG, "RTL-SDR opening device %d", deviceId);
+    if (rx888_open(&dev, deviceId) != 0) {
+        throw std::runtime_error("Unable to open RX888 device");
+    }
+
     
 }
 
